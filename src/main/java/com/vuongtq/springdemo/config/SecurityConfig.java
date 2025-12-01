66bc184f5c37f219534bfb89c6d8720ace0ebf5d
vuongtq <vuongtq@vietis.com.vn>
2025-12-01 11:38:05 +0700

    demo basic authenticate & authority

--- src/main/java/com/vuongtq/springdemo/config/SecurityConfig.java
+++ src/main/java/com/vuongtq/springdemo/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.vuongtq.springdemo.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,21 +11,43 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configurable
+@Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig {
 
     @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin((formLogin) -> formLogin
+                .loginProcessingUrl("/login")
+        );
+
+        http.authorizeHttpRequests(req -> req
+                .requestMatchers("/api/v1/auth/login", "/api/v1/auth/register")
+                .permitAll()
+//                .requestMatchers("/v1/admin/vip").hasAnyRole("ADMIN")
+//                .requestMatchers("/v1/admin/normal").hasAnyRole("ADMIN", "USER")
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+
+    @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails admin = User.withUsername("admin")
-                .password("111")
-                .roles("admin", "user")
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("111"))
+//                .roles("admin", "user")
+                .authorities("ROLE_ADMIN", "ROLE_USER")
                 .build();
 
         UserDetails user = User.withUsername("user")
-                .password("{noop}222")
-                .roles("user")
+                .password(passwordEncoder().encode("222"))
+//                .roles("user")
+                .authorities("ROLE_USER")
                 .build();
 
         return new InMemoryUserDetailsManager(admin, user);
--- src/main/java/com/vuongtq/springdemo/controller/AdminController.java
+++ src/main/java/com/vuongtq/springdemo/controller/AdminController.java
@@ -1,11 +1,16 @@
 package com.vuongtq.springdemo.controller;
 
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/admin")
+@RolesAllowed("ADMIN")
 public class AdminController {
 
     @GetMapping("/vip")
@@ -16,5 +21,10 @@ public class AdminController {
     @GetMapping("/normal")
     public String zoneNormal() {
         return "Normal Zone";
+    }
+
+    @GetMapping("/info")
+    public Authentication getInfoUser() {
+        return SecurityContextHolder.getContext().getAuthentication();
     }
 }
